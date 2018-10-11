7520d3b26cc38f0c90927cee5a86d3463a37789e
Jim Hokanson <jim.hokanson@gmail.com>
2018-10-11 15:41:39 -0400

    Finished trigger example

--- examples/e001_trigger_tests.m
+++ examples/e001_trigger_tests.m
@@ -1,3 +1,5 @@
+%e001_trigger_tests
+
 %Set 10 Hz on chan 1 and 40 Hz on chan 3
 %1) Start them at the same time
 %2) Set them on their own triggers
@@ -9,6 +11,7 @@
 %For 4 channels ...
 %Ideally this would be padded in some way
 %to avoid the zeros ...
+%Basically this says trigger 1 should activate all 4 channels
 map = mcs.utils.bitmask({[1 2 3 4] 0 0 0});
 
 s = mcs.getStimulator();
@@ -18,19 +21,35 @@ amplitude = 1000; %uA
 pt1 = 1000*mcs.getFixedRatePattern(10);
 pt2 = 1000*mcs.getFixedRatePattern(40);
 
+%This apparently sends  the first pattern to all channels
+%which is not what I expeted ... Is this a bug????
+%
+%When run I see pt1 on channels 1 & 3....
 s.setupTrigger('channel_maps',map,'syncout_maps',map)
 
-s.sentDataToDevice(1,pt1,'mirror_to_sync',true);
-%Set pt2 on chan 3
-s.sentDataToDevice(3,pt2,'mirror_to_sync',true);
+s.sentDataToDevice([1 3],{pt1 pt2});
 
 tr = s.trigger_settings;
 disp(tr)
 
 s.setupTrigger('repeat_all',0);
 
+%Stimulate with the current trigger setup
+%---------------------------------------------
+%Not what I expected ... - see above note ...
 s.startStim('triggers',1);
 %Stop all triggers
 s.stopStim();
 
+%Change the trigger setup ...
 s.setupTrigger('linearize',true,'repeat_all',0);
+s.startStim('triggers',1);
+pause(0.5)
+s.startStim('triggers',3);
+%when ready ...
+s.stopStim();
+
+%Start at the same time ...
+s.startStim('triggers',[1 3]);
+s.stopStim();
+
