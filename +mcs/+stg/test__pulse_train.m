c3bc0dd0646eafe72c844257244ed0e48a7e4887
JimHokanson <jim.hokanson@gmail.com>
2019-01-21 12:01:58 -0500

    placeholder for testing

---
+++ +mcs/+stg/test__pulse_train.m
@@ -0,0 +1,13 @@
+classdef (Hidden) test__pulse_train
+
+
+    methods
+        function obj = test__pulse_train()
+            %TODO: Test in.pulses_duration with rate that requires rounding
+            %
+            %i.e. 2 seconds at 22.1Hz
+        end
+    end
+
+
+end
